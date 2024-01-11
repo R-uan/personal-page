59e60969ed261fcb96795c016eb66ed08a22a529
Ruan <ruan.oliveiralopes@hotmail.com>
2024-01-11 10:58:18 -0300

    Create About Me Page

---
+++ src/app/_sliders/AboutMePage.tsx
@@ -0,0 +1,9 @@
+import { UIEventHandler, useRef, useState } from "react";
+
+export default function AboutMePage() {
+	return (
+		<div className="sliders flex justify-center bg-[#0F0F0F]">
+			<div className="bg- w-[90%] w-80%"></div>
+		</div>
+	);
+}
