f2ea4e8481d722cd5dde5d5d0fc767f2a7e15e01
Ruan <ruan.oliveiralopes@hotmail.com>
2024-01-17 10:14:19 -0300

    Project Page Almost Done
    - All media queries
    - Need to do the repository component with actual logic

--- src/app/_sliders/Projects.tsx
+++ src/app/_sliders/Projects.tsx
@@ -4,18 +4,26 @@ import "../../style/media-queries/media-projects.css";
 export default function AboutMePage() {
 	const tags = ["react", "next", "nodejs", "tailwind", "typescript"];
 	return (
-		<div id="about-me" className="sliders flex justify-center bg-[#161616] gap-[40px]">
-			<div className="invisible testclass"></div>
+		<div id="about-me" className="sliders flex justify-center bg-[#161616] gap-[10px]">
 			<div className="experiences mt-[64px] p-2 h-[90vh] flex flex-col gap-3 overflow-auto">
+				<span>
+					<h1>Featured Projects</h1>
+				</span>
 				<div className="experience-professional items-center pb-5 w-fit h-fit">
-					<span className="text-4xl font-serif w-m">Professional</span>
+					<span className="w-full mb-[2vh]">
+						<h1 className="w-m">Professional</h1>
+					</span>
 					<div className="repositories">
+						<TestComponent />
+						<TestComponent />
+						<TestComponent />
 						<TestComponent />
 					</div>
 				</div>
+				<hr />
 				<div className="experience-personal relative w-fit h-fit">
 					<span className="w-full mb-[2vh]">
-						<h1 className="text-4xl w-m">Personal Projects</h1>
+						<h1 className="w-m">Personal</h1>
 					</span>
 					<div className="repositories">
 						<TestComponent />
