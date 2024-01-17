3d112d1671f76348838f9644cb63cdd1bdf30ba1
Ruan <ruan.oliveiralopes@hotmail.com>
2024-01-17 02:15:43 -0300

    style changes

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 export default function Header() {
 	return (
-		<header className="flex mobile:justify-center mobile:left-0 absolute w-full left-[7vw] mt-2 flex-row h-[7vh] z-50">
+		<header className="flex mobile:justify-center items-center mobile:left-0 absolute w-[100vw] left-[7vw] mt-2 flex-row h-[8vh] z-50">
 			<div className="text-3xl absolute">
 				<h1 id="header-logo">LOGO</h1>
 			</div>
