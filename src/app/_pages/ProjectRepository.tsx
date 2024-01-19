e88fddad47ef81f2c055346d4b9c4f26f0b8a25b
Ruan <ruan.oliveiralopes@hotmail.com>
2024-01-19 14:26:46 -0300

    repository component

--- src/app/_pages/ProjectRepository.tsx
+++ src/app/_pages/ProjectRepository.tsx
@@ -1,8 +1,15 @@
+import { UseColorThemeContext } from "../_context/ColorThemeContext";
+
 export default function TestComponent() {
+	const { CurrentTheme } = UseColorThemeContext();
 	const tags = ["react", "next", "nodejs", "tailwind", "typescript"];
 	return (
 		<div className="repository flex flex-col h-fit w-max">
-			<h3 className="text-4xl text-[#d0d0d0] font-semibold mb-[5px]">Portfolio</h3>
+			<h3
+				style={{ color: CurrentTheme.Heading2 }}
+				className="text-4xl text-[#d0d0d0] font-semibold mb-[5px]">
+				Portfolio
+			</h3>
 			<p className="text-[#F4EAE0] mb-[1.5vh] text-lg">
 				Attempt to make a portfolio for a web developer (me) with Next JS (Node JS, React,
 				Tailwind).Attempt to make a portfolio for a web developer (me) with Next JS (Node
@@ -10,9 +17,15 @@ export default function TestComponent() {
 				JS (Node JS, React, Tailwind).
 			</p>
 			<ul className="flex gap-1 repo-tags flex-wrap">
-				{tags.map((tag) => {
+				{tags.map((tag, index) => {
 					return (
-						<li className="p-3 pt-1 pb-1 w-fit text-2xl text-[#000000cc] bg-[#F6F7C4] rounded-2xl">
+						<li
+							key={index}
+							style={{
+								background: CurrentTheme.IconBackground,
+								color: CurrentTheme.Paragraph,
+							}}
+							className="p-3 pt-1 pb-1 w-fit text-2xl rounded-2xl">
 							{tag}
 						</li>
 					);
